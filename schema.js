5e2dbd3e1561d7a8e0da24914cb61ebee8cc03c9
Matthew J Whitney <matthewjwhitney@gmail.com>
2019-12-09 01:16:35 -0700

    remove products from schema for now

--- schema.js
+++ schema.js
@@ -1,17 +1,7 @@
 const { gql } = require("apollo-server-express");
 const mongoose = require("mongoose");
-/* const producers = require("./producers.json"); */
-/* const products = require("./products.json"); */
 
 const typeDefs = gql`
-  #type Product {
-  #  description: String
-  #  unit: String
-  #  price: String
-  #  producer: String
-  #  notes: String
-  #}
-
   type Producer {
     name: String
     location: String
@@ -25,18 +15,9 @@ const typeDefs = gql`
 
   type Query {
     getProducers: [Producer]
-    # getProducts: [Product]
   }
 
   type Mutation {
-    # addProduct(
-    #   description: String
-    #   unit: String
-    #   price: String
-    #   producer: String
-    #   notes: String
-    # ): Product
-
     addProducer(
       name: String
       location: String
@@ -67,7 +48,6 @@ const MProducers = mongoose.model("Producer", producerSchema);
 
 const resolvers = {
   Query: {
-    /* getProducts: () => products */
     getProducers: () => {
       return MProducers.find();
     }
